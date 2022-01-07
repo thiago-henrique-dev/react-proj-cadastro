131b3e6bcd0a85b36de1a8479da405bc0e63576e
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-07 13:35:59 -0300

    Componente Tabela #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from '../core/Cliente'
 
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 20, '2'),
+    new Cliente('Carlos', 18, '3'),
+    new Cliente('Thiago', 17, '4'),
+
+  ]
+
   return (
     <div className={`
         flex justify-center items-center h-screen
         bg-gradient-to-r from-blue-500 to-purple-500
       `}>
         <Layout titulo="Cadastro simples">
-            <span>Conteudo</span>
+            <Tabela clientes={clientes}></Tabela>
         </Layout>
     </div>
   )

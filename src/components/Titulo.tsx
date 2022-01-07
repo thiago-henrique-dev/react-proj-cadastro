a8e3175d59a90faa7617f77d114a051758b14b05
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-07 13:03:28 -0300

    Componentes Layout e Título

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,9 @@
+export default function Titulo(props){
+    return (
+        <div className="flex flex-col justify-center">
+            <h1 className="pl-5 py-2 text-2x1">
+                {props.children}</h1>
+            <hr className="border-2 border-purple-500"/>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
+import Layout from "../components/Layout";
 
 
 export default function Home() {
   return (
     <div className={`
-       flex h-screen justify-center items-center
-       bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
-       `}>
-      <span className='text-4xl'>Texto</span>
+        flex justify-center items-center h-screen
+        bg-gradient-to-r from-blue-500 to-purple-500
+      `}>
+        <Layout titulo="Cadastro simples">
+            <span>Conteudo</span>
+        </Layout>
     </div>
   )
 }

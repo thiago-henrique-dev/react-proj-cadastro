3e34bb453b231adb831e96265394a44e0115f05e
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-07 12:31:34 -0300

    Configuração: NextJS, TailwindCSS e Firebase

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className={`
+       flex h-screen justify-center items-center
+       bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+       `}>
+      <span className='text-4xl'>Texto</span>
+    </div>
+  )
+}
